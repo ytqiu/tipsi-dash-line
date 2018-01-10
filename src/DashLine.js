bd2730b30c1ee8151de5610f5cb445c42ddbf1f8
邱亚涛 <qiuyatao@117sport.com>
2018-01-10 18:06:36 +0800

    proptypes update

--- src/DashLine.js
+++ src/DashLine.js
@@ -1,23 +1,22 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import { View, requireNativeComponent } from 'react-native'
 
 export default class DashLine extends Component {
-  static defaultProps = {
-    horizontal: true,
-  }
-  static propTypes = {
-    ...View.propTypes,
-    dashColor: PropTypes.string,
-    dashLineWidth: PropTypes.number,
-    dashLineSpace: PropTypes.number,
-    horizontal: PropTypes.bool,
-  }
+    static defaultProps = {
+        horizontal: true
+    }
+    static propTypes = {
+        ...View.propTypes,
+        dashColor: PropTypes.string,
+        dashLineWidth: PropTypes.number,
+        dashLineSpace: PropTypes.number,
+        horizontal: PropTypes.bool
+    }
 
-  render() {
-    return (
-      <DashLineComponent {...this.props} />
-    )
-  }
+    render() {
+        return <DashLineComponent {...this.props} />
+    }
 }
 
 const DashLineComponent = requireNativeComponent('DashLineComponent', DashLine)
